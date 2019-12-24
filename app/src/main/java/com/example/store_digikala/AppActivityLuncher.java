7c65f58dd91e39b4b4eb4ca78d35129793e1fd47
omid <omidtkd77@gmail.com>
2019-12-24 15:42:22 +0330

    create MainFragment and Single Fragment Activity

--- app/src/main/java/com/example/store_digikala/AppActivityLuncher.java
+++ app/src/main/java/com/example/store_digikala/AppActivityLuncher.java
@@ -2,50 +2,13 @@ package com.example.store_digikala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.Toast;
-
-import com.airbnb.lottie.LottieAnimationView;
 
 public class AppActivityLuncher extends AppCompatActivity {
-
-
-    private LottieAnimationView lottieAnimationView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luncher);
-        lottieAnimationView=findViewById(R.id.animation);
-        lottieAnimationView.playAnimation();
-
-        Handler handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                lottieAnimationView.pauseAnimation();
-                if (isNetworkAvailable()) {
-                    Intent intent = new Intent(AppActivityLuncher.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(AppActivityLuncher.this, "Please check your internet :(", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            }
-        },4000);
-
     }
-
-    private boolean isNetworkAvailable() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
-        boolean isNetworkcConnected = isNetworkAvailable &&
-                cm.getActiveNetworkInfo().isConnected();
-
-        return isNetworkcConnected;
-    }}
+}
