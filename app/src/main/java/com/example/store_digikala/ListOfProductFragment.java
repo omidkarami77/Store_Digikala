ef26fe9494fb835e96562191c50871f7d96ef48f
omid <omidtkd77@gmail.com>
2020-01-21 12:22:37 +0330

    edit and complete listOfProductFragment

--- app/src/main/java/com/example/store_digikala/ListOfProductFragment.java
+++ app/src/main/java/com/example/store_digikala/ListOfProductFragment.java
@@ -1,18 +1,25 @@
 package com.example.store_digikala;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.store_digikala.model.Products;
 import com.example.store_digikala.network.Api;
 import com.example.store_digikala.network.RetrofitClientInstance;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -28,7 +35,7 @@ public class ListOfProductFragment extends Fragment {
 
     private static final String ARG_GALLERY = "requestItemProduct";
     private RecyclerView mRecyclerView;
- //   private ProductAdapter mAdapter;
+    private ProductAdapter mAdapter;
 
     private int requsetCode;
 
@@ -50,23 +57,135 @@ public class ListOfProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_list_of_product, container, false);
- mRecyclerView=view.findViewById(R.id.recycler_navigarion_item_product);
- if (requsetCode==1){
-     RetrofitClientInstance.getRetrofitInstance().create(Api.class).getPopularity().enqueue(new Callback<List<Products>>() {
-         @Override
-         public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
-           if (response.isSuccessful()){
-               List<Products>productsList=response.body();
-           }
-         }
+        View view = inflater.inflate(R.layout.fragment_list_of_product, container, false);
+        mRecyclerView = view.findViewById(R.id.recycler_navigarion_item_product);
+        if (requsetCode == 1) {
+            RetrofitClientInstance.getRetrofitInstance().create(Api.class).getPopularity().enqueue(new Callback<List<Products>>() {
+                @Override
+                public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
+                    if (response.isSuccessful()) {
+                        List<Products> productsList = response.body();
+                        mAdapter = new ProductAdapter(productsList);
+                        mRecyclerView.setAdapter(mAdapter);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-         @Override
-         public void onFailure(Call<List<Products>> call, Throwable t) {
+                    }
+                }
 
-         }
-     });
- }
+                @Override
+                public void onFailure(Call<List<Products>> call, Throwable t) {
+
+                }
+            });
+        } else if (requsetCode == 2) {
+            RetrofitClientInstance.getRetrofitInstance().create(Api.class).getTBestSelerList().enqueue(new Callback<List<Products>>() {
+                @Override
+                public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
+                    if (response.isSuccessful()) {
+                        List<Products> productsList = response.body();
+                        mAdapter = new ProductAdapter(productsList);
+                        mRecyclerView.setAdapter(mAdapter);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    }
+                }
+
+
+                @Override
+                public void onFailure(Call<List<Products>> call, Throwable t) {
+
+                }
+            });
+        } else if (requsetCode == 3) {
+            RetrofitClientInstance.getRetrofitInstance().create(Api.class).getProductList().enqueue(new Callback<List<Products>>() {
+                @Override
+                public void onResponse(Call<List<Products>> call, Response<List<Products>> response) {
+                    if (response.isSuccessful()) {
+                        List<Products> productsList = response.body();
+                        mAdapter = new ProductAdapter(productsList);
+                        Log.e("TAG", productsList.size() + "");
+                        mRecyclerView.setAdapter(mAdapter);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Products>> call, Throwable t) {
+
+                }
+            });
+        }
+        return view;
+    }
+
+    private class ProductHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mImageView;
+        private TextView mTextViewName;
+        private TextView mTextViewPrice;
+        private Products mProducts;
+
+        public ProductHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.image_view_search_result);
+            mTextViewName = itemView.findViewById(R.id.text_name_product_search);
+            mTextViewPrice = itemView.findViewById(R.id.text_price_search_item);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = ProductInformationActivity.newInent(getActivity(), mProducts.getId());
+                    startActivity(intent);
+                }
+            });
+        }
+
+        private void bind(Products products) {
+            mProducts = products;
+            mTextViewName.setText(products.getName());
+            mTextViewPrice.setText(products.getPrice());
+            if (mProducts.getImages() != null && mProducts.getImages().size() > 0) {
+                Picasso.get().load(mProducts.getImages().get(0).getPath()).fit().centerCrop().into(mImageView);
+            }
+        }
+
+    }
+
+    private class ProductAdapter extends RecyclerView.Adapter<ProductHolder> {
+        private List<Products> productsList;
+
+        public void setProductsList(List<Products> productsList) {
+            this.productsList = productsList;
+        }
+
+        public List<Products> getProductsList() {
+            return productsList;
+        }
+
+        private ProductAdapter(List<Products> productsList) {
+            this.productsList = productsList;
+        }
+
+        @NonNull
+        @Override
+        public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            View view = inflater.inflate(R.layout.search_item_product, parent, false);
+            return new ProductHolder(view);
+
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
+            Products products = productsList.get(position);
+            holder.bind(products);
+        }
+
+        @Override
+        public int getItemCount() {
+            return productsList.size();
+        }
     }
 
 }
+
